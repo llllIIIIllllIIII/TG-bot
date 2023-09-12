0cd2705467066dfb2f22d3aa52c4365d4cb5f966
llllIIIIllllIIII <henrywinnie321@yahoo.com.tw>
2023-09-12 11:54:48 +0800

    add get_block_height function

--- BlockHeight.py
+++ BlockHeight.py
@@ -1,4 +1,13 @@
 import requests
-paylaod = {"query": "{wcBlocks: blocks(workchain: 0, page_size: 1) { seqno }}"}
-r = requests.post('https://dton.io/graphql/',data = paylaod )
-r.text
+import json
+
+
+def get_block_height():
+    paylaod = {"query": "{wcBlocks: blocks(workchain: 0, page_size: 1) { seqno }}"}
+    r = requests.post('https://dton.io/graphql/',data = paylaod )
+    data = json.loads(r.text)
+    blockheight = data['data']['wcBlocks'][0]['seqno']
+    return blockheight
+
+
+
